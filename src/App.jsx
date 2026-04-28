77886f422f0aa3b387da30b405f009d4beb1f5bd
chandru90 <iochandru90@gmail.com>
2026-04-28 06:09:43 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Internships from "./components/Internships";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+
 import Certifications from "./components/certifications";
 const App = () => {
   return (
